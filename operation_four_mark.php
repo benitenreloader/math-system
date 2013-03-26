ecb49ad4bb107d24e5f0707cc59b4b2710eb9788
Mark Edward Tan <mark.tan@reloadmedia.com.au>
2013-03-25 17:42:18 -0700

    added operation four file

--- operation_four_mark.php
+++ operation_four_mark.php
@@ -3,7 +3,7 @@
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
 
-require 'mathoperation.class.php';
+require 'library/mathoperation.class.php';
 
 class OperationFourCustom extends OperationFour
 {
