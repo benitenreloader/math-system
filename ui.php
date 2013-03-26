d9b9bc370b0c082fb5d2fc146f0b62c02e96c3b3
madizza <madizza.colico@reloadmedia.com.au>
2013-03-26 11:42:08 -0700

    [#46875387] -iz

---
+++ ui.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title> Operation One</title>
+</head>
+<body>
+<form action='operation_one.php' method='post'>
+Enter the values for operation 1: <br />
+Variable 1:<input type='text' name='txt1' /> <br /><br />
+Variable 2:<input type='text' name='txt2' /><br /><br />
+<input type='submit' name='submit' value='submit'/>
+</form>
+</body>
+</html>
