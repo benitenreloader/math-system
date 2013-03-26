8ecc9835bc007ec7548b4a3b950f6148372079d8
Mark Edward Tan <mark.tan@reloadmedia.com.au>
2013-03-25 17:20:06 -0700

    [#46786217] added file commit

---
+++ library/operation_four_mark.php
@@ -0,0 +1,34 @@
+<?php
+
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+require 'mathoperation.class.php';
+
+class OperationFourCustom extends OperationFour
+{
+	function __construct($a, $b, $c, $d, $e) {
+		$this->a = $a;
+		$this->b = $b;
+		$this->c = $c;
+		$this->d = $d;
+		$this->e = $e;
+	}
+	
+	public function multiplication() {
+		return $this->a * $this->b * $this->c * $this->d * $this->e;
+	}
+	
+	public function subtraction() {
+		return $this->a - $this->b - $this->c - $this->d - $this->e;
+	}
+}
+
+$obj = new OperationFourCustom(20, 3, 2, 4, 1);
+
+$product = $obj->multiplication();
+echo $product . '<hr />';
+
+$difference = $obj->subtraction();
+echo $difference;
+?>
